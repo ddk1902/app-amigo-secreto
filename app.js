8726fd10ac5463377635bfd738cfc7d71072a204
Diego Gomez <gomezdiego1902@gmail.com>
2025-01-21 23:54:21 -0400

    Creación de la función sortearAmigo

--- app.js
+++ app.js
@@ -20,4 +20,34 @@ function agregarAmigo() {
         return;
     }
 
+// Agregamos el nombre a la lista
+amigos.push(nombre);
+const listItem = document.createElement("li");
+listItem.textContent = nombre;
+listaAmigos.appendChild(listItem);
+
+// Limpiamos el input
+input.value = "";
+input.focus();
+}
+
+// Función para sortear el amigo secreto
+function sortearAmigo() {
+const resultado = document.getElementById("resultado");
+resultado.innerHTML = "";
+
+// Verificar si el número de amigos es par
+if (amigos.length < 2) {
+    alert("Se necesitan al menos dos amigos para realizar el sorteo.");
+    return;
+}
+
+if (amigos.length % 2 !== 0) {
+    alert("El número de amigos debe ser par para que nadie quede sin amigo secreto..!!!");
+    return;
+}
+
+// Creamos una copia del array para sortear
+const nombresRestantes = [...amigos];
+const sorteados = {};
 }
